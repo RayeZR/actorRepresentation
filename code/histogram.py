d2bdf3736f3293525f1622177ab63b11589ae346
RayeZR <rui.zhang1@etu.univ-cotedazur.fr>
2020-07-26 20:38:45 +0200

    histogram and voodoo features

--- code/histogram.py
+++ code/histogram.py
@@ -2,94 +2,86 @@ import cv2
 import os
 import numpy as np
 import copy
+import matplotlib.pyplot as plt
+
 
 
 class Solver:
-    def __init__(self, inp_file):
+    def __init__(self, inp_file, out_dir):
         self.inp_file = inp_file
         self.cap = cv2.VideoCapture(inp_file)
         self.width, self.height = (int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        # self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        # self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
         self.filename = os.path.splitext(inp_file)[0].split(os.sep)[-1]
+        self.out_dir = out_dir + os.sep + self.filename
+        self.rgb_dir = self.out_dir + '_RGB'
+        self.l_dir = self.out_dir + '_L'
+        if not os.path.exists(self.rgb_dir):
+            os.makedirs(self.rgb_dir)
+        if not os.path.exists(self.l_dir):
+            os.makedirs(self.l_dir)
 
-    def show_hist(hist):
+    def show_hist(self, hist, color):
         bin_count = hist.shape[0]
-        bin_w = 24
+        bin_w = 1
         img = np.zeros((256, bin_count * bin_w, 3), np.uint8)
         for i in range(bin_count):
             h = int(hist[i])
-            cv2.rectangle(img, (i * bin_w + 2, 255), ((i + 1) * bin_w - 2, 255 - h), (int(180.0 * i / bin_count), 255, 255),
-                         -1)
-        img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+            # cv2.rectangle(img, (i * bin_w + 2, 255), ((i + 1) * bin_w - 2, 255 - h), (int(180.0 * i / bin_count), 255, 255), -1)
+            # cv2.rectangle(img, (i * bin_w + 2, 255), ((i + 1) * bin_w - 2, 255 - h), (255, 255, 255), -1)
+            cv2.circle(img, (i, 255-h), 1, color, 2)
+        # img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
         cv2.imshow('hist', img)
 
     def get_hist(self, num_bin):
+        channels = ("b", "g", "r")
         ret, frame = self.cap.read()
-        self.features = []
+        f_id = 0
         while (frame is not None):
-            img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            hue = img_hsv[:, :, 0]
             mask = np.ones([self.height, self.width], dtype=np.uint8)
-            hist = cv2.calcHist([img_hsv], [0], mask, [num_bin], [0, 255])
-            cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
-            hist = hist.reshape(-1)
-            self.features.append(hist)
+            fig = plt.figure()
+            for ch in range(3):
+                hist = cv2.calcHist([frame], [ch], mask, [num_bin], [0, 255])
+                # cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+                plt.plot(hist, color=channels[ch], label=channels[ch])
+                # self.show_hist(hist, colors[ch])
+            plt.legend()
+            # plt.show()
+            fig.savefig(self.rgb_dir + os.sep + 'hist_rgb_%d.png' % f_id)
+            plt.close(fig)
+
+            fig = plt.figure()
+            hist = cv2.calcHist([cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)], [2], mask, [num_bin], [0, 255])
+            plt.plot(hist, color='c', label='V-HSV')
+
+            hist = cv2.calcHist([cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)], [1], mask, [num_bin], [0, 255])
+            plt.plot(hist, color='m', label='L-HSL')
+
+            ave = (frame[:, :, 0] + frame[:, :, 1] + frame[:, :, 2]) / 3
+            hist = cv2.calcHist([ave.astype(np.uint8)], [0], mask, [num_bin], [0, 255])
+            plt.plot(hist, color='y', label='I-ave(R,G,B)')
+
+            luma = 0.0722 * frame[:, :, 0] + 0.7152 * frame[:, :, 1] + 0.2126 * frame[:, :, 2]
+            hist = cv2.calcHist([luma.astype(np.uint8)], [0], mask, [num_bin], [0, 255])
+            plt.plot(hist, color='k', label='luma-HDTV(BT709)')
+
+            plt.legend()
+            # plt.show()
+            fig.savefig(self.l_dir + os.sep + 'hist_l_%d.png' % f_id)
+            plt.close(fig)
+
+            f_id += 1
             ret, frame = self.cap.read()
-        # self.features.pop(-1) # remove the last frame
 
-    def split_shots(self, num_center):
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        ret, self.labels, centers = cv2.kmeans(np.array(self.features), num_center, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-        print("length: %d" % len(self.labels.reshape(-1)))
-        # print(self.labels.reshape(-1))
-
-    def correct_labels(self):
-        num_frame = len(self.labels)
-        for i in range(num_frame):
-            if i == 0 or i == (num_frame - 1):
-                continue
-            if self.labels[i - 1] != self.labels[i] and self.labels[i] != self.labels[i + 1]:
-                self.labels[i] = self.labels[i - 1]
-
-    def write(self, out_dir):
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-        self.cap = cv2.VideoCapture(self.inp_file)
-        idx = 0
-        label = self.labels[0]
-        writer = cv2.VideoWriter(os.path.join(out_dir, self.filename) + '_%03d.avi' % idx, self.fourcc, self.fps, (self.width, self.height))
-        ret, frame = self.cap.read()
-        i = 0
-        info = '000.avi: '
-        while frame is not None:
-            if self.labels[i] == label:
-                writer.write(frame)
-                info += '%d ' % self.labels[i]
-            else:
-                idx += 1
-                writer.release()
-                writer = cv2.VideoWriter(os.path.join(out_dir, self.filename) + '_%03d.avi' % idx, self.fourcc, self.fps, (self.width, self.height))
-                writer.write(frame)
-                print(info)
-                info = '%03d.avi: %d ' % (idx, self.labels[i])
-            label = self.labels[i]
-            i += 1
-            # print(i)
-            ret, frame = self.cap.read()
-        writer.release()
+        self.cap.release()
 
 
 
 if __name__ == '__main__':
-    # inp_file = r'E:\I3S\actorRepresentation\data\PySceneDetect\03\03-Scene-008.mp4'
-    inp_file = r'E:\I3S\actorRepresentation\data\PySceneDetect\04\04-Scene-001.mp4'
-    out_dir = r'E:\I3S\actorRepresentation\data\PySceneDetect\hist_test_04-001'
+    inp_file = r'E:\I3S\actorRepresentation\code\test_data1\03-Scene-016.mp4'
+    out_dir = r'E:\I3S\actorRepresentation\code\histogram'
     # num_bin = 16
     num_bin = 256
-    num_center = 6
-    solver = Solver(inp_file)
+    solver = Solver(inp_file, out_dir)
     solver.get_hist(num_bin)
-    solver.split_shots(num_center)
-    solver.correct_labels()
-    solver.write(out_dir)
---
+++ code/split_by_hist.py
@@ -0,0 +1,95 @@
+import cv2
+import os
+import numpy as np
+import copy
+
+
+class Solver:
+    def __init__(self, inp_file):
+        self.inp_file = inp_file
+        self.cap = cv2.VideoCapture(inp_file)
+        self.width, self.height = (int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.filename = os.path.splitext(inp_file)[0].split(os.sep)[-1]
+
+    def show_hist(hist):
+        bin_count = hist.shape[0]
+        bin_w = 24
+        img = np.zeros((256, bin_count * bin_w, 3), np.uint8)
+        for i in range(bin_count):
+            h = int(hist[i])
+            cv2.rectangle(img, (i * bin_w + 2, 255), ((i + 1) * bin_w - 2, 255 - h), (int(180.0 * i / bin_count), 255, 255),
+                         -1)
+        img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+        cv2.imshow('hist', img)
+
+    def get_hist(self, num_bin):
+        ret, frame = self.cap.read()
+        self.features = []
+        while (frame is not None):
+            img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hue = img_hsv[:, :, 0]
+            mask = np.ones([self.height, self.width], dtype=np.uint8)
+            hist = cv2.calcHist([img_hsv], [0], mask, [num_bin], [0, 255])
+            cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+            hist = hist.reshape(-1)
+            self.features.append(hist)
+            ret, frame = self.cap.read()
+        # self.features.pop(-1) # remove the last frame
+
+    def split_shots(self, num_center):
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret, self.labels, centers = cv2.kmeans(np.array(self.features), num_center, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        print("length: %d" % len(self.labels.reshape(-1)))
+        # print(self.labels.reshape(-1))
+
+    def correct_labels(self):
+        num_frame = len(self.labels)
+        for i in range(num_frame):
+            if i == 0 or i == (num_frame - 1):
+                continue
+            if self.labels[i - 1] != self.labels[i] and self.labels[i] != self.labels[i + 1]:
+                self.labels[i] = self.labels[i - 1]
+
+    def write(self, out_dir):
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        self.cap = cv2.VideoCapture(self.inp_file)
+        idx = 0
+        label = self.labels[0]
+        writer = cv2.VideoWriter(os.path.join(out_dir, self.filename) + '_%03d.avi' % idx, self.fourcc, self.fps, (self.width, self.height))
+        ret, frame = self.cap.read()
+        i = 0
+        info = '000.avi: '
+        while frame is not None:
+            if self.labels[i] == label:
+                writer.write(frame)
+                info += '%d ' % self.labels[i]
+            else:
+                idx += 1
+                writer.release()
+                writer = cv2.VideoWriter(os.path.join(out_dir, self.filename) + '_%03d.avi' % idx, self.fourcc, self.fps, (self.width, self.height))
+                writer.write(frame)
+                print(info)
+                info = '%03d.avi: %d ' % (idx, self.labels[i])
+            label = self.labels[i]
+            i += 1
+            # print(i)
+            ret, frame = self.cap.read()
+        writer.release()
+
+
+
+if __name__ == '__main__':
+    # inp_file = r'E:\I3S\actorRepresentation\data\PySceneDetect\03\03-Scene-008.mp4'
+    inp_file = r'E:\I3S\actorRepresentation\data\PySceneDetect\04\04-Scene-001.mp4'
+    out_dir = r'E:\I3S\actorRepresentation\data\PySceneDetect\hist_test_04-001'
+    # num_bin = 16
+    num_bin = 256
+    num_center = 6
+    solver = Solver(inp_file)
+    solver.get_hist(num_bin)
+    solver.split_shots(num_center)
+    solver.correct_labels()
+    solver.write(out_dir)
